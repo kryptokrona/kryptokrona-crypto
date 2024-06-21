bc86314fa45b83407f224d00533299db5ccb6b5a
mjovanc <mjovanc@icloud.com>
2024-06-21 10:24:09 +0200

    Updating build to generate wasm

--- build_js.sh
+++ build_js.sh
@@ -14,7 +14,7 @@ cd ..
 patch -N --verbose emsdk/fastcomp/emscripten/src/shell.js scripts/emscripten.patch
 
 mkdir -p jsbuild && cd jsbuild && rm -rf *
-emconfigure cmake .. -DNO_AES=1 -DARCH=default -DBUILD_WASM=1 -DBUILD_JS=0
+emcmake cmake .. -DNO_AES=1 -DARCH=default -DBUILD_WASM=1 -DBUILD_JS=0
 make && cp turtlecoin-crypto-wasm.js ../dist
-emconfigure cmake .. -DNO_AES=1 -DARCH=default -DBUILD_WASM=0 -DBUILD_JS=1
+emcmake cmake .. -DNO_AES=1 -DARCH=default -DBUILD_WASM=0 -DBUILD_JS=1
 make && cp turtlecoin-crypto.js ../dist
--- emsdk/test.sh
+++
@@ -1,6 +0,0 @@
-echo "test the standard workflow (as close as possible to how a user would do it, in the shell)"
-./emsdk install latest
-./emsdk activate latest
-source ./emsdk_env.sh --build=Release
-emcc -v
-
